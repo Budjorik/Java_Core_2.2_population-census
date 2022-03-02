7b52ebff665512a95a00ab3abbd7815add069a8b
Сергей Караульных <karaulnih@gmail.com>
2022-03-03 01:36:51 +0500

    Create files

---
+++ Education.java
@@ -0,0 +1,8 @@
+package ru.netology.lesson1;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ Sex.java
@@ -0,0 +1,6 @@
+package ru.netology.lesson1;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
